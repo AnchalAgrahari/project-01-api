ec0a5e11ceb0e626163a35c2d9ffa8bc15bf5486
Anchal Agrahari <anchalagrahari258@gmail.com>
2025-11-27 06:01:11 +0530

    Creating item route

---
+++ project-03/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from database import Base, engine, SessionLocal 
+from models import Item
+from schemas import ItemCreate
+import schemas
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post("/items/", response_model=schemas.Item)
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    db_item = Item(name=item.name, description=item.description)
+    db.add(db_item)
+    db.commit()
+    db.refresh(db_item)
+    return db_item 
